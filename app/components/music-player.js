1778a5ff552334b7c6293edd0061a3ef98734531
jarosenthal <jrosenthal96@gmail.com>
2018-02-02 14:18:33 -0500

    Progress bar

--- app/components/music-player.js
+++ app/components/music-player.js
@@ -1,5 +1,20 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    classNames: ['music-player']
+  classNames: ['music-player'],
+  position: '1:32',
+  length: '3:16',
+  progress: 50,
+  actions: {
+    pickTime(e) {
+      const $target = $(".progress-bar"),
+        width = $target.width(),
+        position = e.offsetX,
+        newWidth = position / width * 100;
+
+      console.log(newWidth);
+
+      $target.find('.progress').css('width', newWidth + "%");
+    }
+  }
 });
