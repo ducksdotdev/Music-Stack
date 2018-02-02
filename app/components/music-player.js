418730123bdf927688742e5d9654e1e9a97229d0
jarosenthal <jrosenthal96@gmail.com>
2018-02-02 16:14:42 -0500

    Fake forward & back actions

--- app/components/music-player.js
+++ app/components/music-player.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['music-player'],
-  length: 196,
+  length: Math.random() * (359 - 140) + 140,
   progress: 0,
   interval: null,
   paused: false,
@@ -17,18 +17,6 @@ export default Component.extend({
         if (progress >= __self.get('length')) clearInterval(__self.get('interval'));
         __self.set('progress', progress + .25);
       }, 250));
-    }
-  },
-  actions: {
-    pickTime(e) {
-      const $target = $(".progress-bar"),
-        width = $target.width(),
-        position = e.offsetX,
-        percentThrough = position / width * 100;
-      this.set('progress', this.get('length') * percentThrough / 100);
-    },
-    pausePlay() {
-      this.set('paused', !this.get('paused'))
     }
   },
   togglePausePlay: function () {
@@ -49,5 +37,24 @@ export default Component.extend({
   setProgress: function () {
     const width = this.get('progress') / this.get('length') * 100;
     $('.progress-bar .progress').css('width', width + "%");
-  }.observes('progress').on('didRender')
+  }.observes('progress').on('didRender'),
+  actions: {
+    pickTime(e) {
+      const $target = $(".progress-bar"),
+        width = $target.width(),
+        position = e.offsetX,
+        percentThrough = position / width * 100;
+      this.set('progress', this.get('length') * percentThrough / 100);
+    },
+    pausePlay() {
+      this.set('paused', !this.get('paused'))
+    },
+    goNext() {
+      this.set('progress', 0);
+      this.set('length', Math.random() * (359 - 140) + 140)
+    },
+    goPrevious() {
+      this.set('progress', 0);
+    }
+  },
 });
