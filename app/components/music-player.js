4627844d93eb86aa197455ebdbb32ba6021066c0
jarosenthal <jrosenthal96@gmail.com>
2018-02-02 14:47:36 -0500

    Set time on render

--- app/components/music-player.js
+++ app/components/music-player.js
@@ -5,15 +5,22 @@ export default Component.extend({
   position: '1:32',
   length: '3:16',
   progress: 50,
+  didRender() {
+    let __self = this;
+    __self._super(...arguments);
+    const length = __self.get('length').split(":"),
+      position = __self.get('position').split(":"),
+      totalTime = (length[0] * 60) + parseInt(length[1]),
+      curTime = (position[0] * 60) + parseInt(position[1]),
+      time = (curTime / totalTime * 100);
+    $('.progress-bar .progress').css('width', time + "%");
+  },
   actions: {
     pickTime(e) {
       const $target = $(".progress-bar"),
         width = $target.width(),
         position = e.offsetX,
         newWidth = position / width * 100;
-
-      console.log(newWidth);
-
       $target.find('.progress').css('width', newWidth + "%");
     }
   }
