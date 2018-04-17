d0d62ae58938dcc6121b09cab293e981b909f579
justin <jrosenthal96@gmail.com>
2018-04-17 16:36:28 -0400

    Friends list

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      "John Doe",
+      "Jane Doe",
+      "Sam Smith",
+      "Dan Davids",
+      "Jacob Worth",
+      "Max Smith",
+      "Brian Long"
+    ];
+  }
 });
